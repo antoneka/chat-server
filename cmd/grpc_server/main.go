3559dc089003144669c17bc39b6714c3d67259a1
antoneka <ku-9a@mail.ru>
2024-02-19 19:37:41 +0300

    feat: add an empty server implementation

--- cmd/grpc_server/main.go
+++ cmd/grpc_server/main.go
@@ -1,7 +1,79 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"math/rand"
+	"net"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"google.golang.org/protobuf/types/known/emptypb"
+
+	desc "github.com/antoneka/chat-server/pkg/chat_v1"
+)
+
+const grpcPort = 443
+
+type server struct {
+	desc.UnimplementedChatV1Server
+}
+
+// Create creates a new chat.
+func (s *server) Create(
+	ctx context.Context,
+	req *desc.CreateRequest,
+) (*desc.CreateResponse, error) {
+	_, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	fmt.Printf("CreateRequest: %+v\n", req)
+
+	return &desc.CreateResponse{
+		Id: rand.Int63(),
+	}, nil
+}
+
+// Delete deletes the chat from the system.
+func (s *server) Delete(
+	ctx context.Context,
+	req *desc.DeleteRequest,
+) (*emptypb.Empty, error) {
+	_, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	fmt.Printf("DeleteRequest: %+v\n", req)
+
+	return &emptypb.Empty{}, nil
+}
+
+// SendMessage sends a message to the server.
+func (s *server) SendMessage(
+	ctx context.Context,
+	req *desc.SendMessageRequest,
+) (*emptypb.Empty, error) {
+	_, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	fmt.Printf("SendMessageRequest: %+v\n", req)
+
+	return &emptypb.Empty{}, nil
+}
 
 func main() {
-	fmt.Println("hello")
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%v", grpcPort))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	reflection.Register(s)
+	desc.RegisterChatV1Server(s, &server{})
+
+	log.Printf("server listening at %v", lis.Addr())
+
+	if err = s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve the server: %v", err)
+	}
 }
